f6840412ef9f75e8695fdec88f7fd9da4d003d42
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-06-20 10:55:01 +0900

    함수

---
+++ C문법/함수/함수.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int sum(int x, int y)
+{
+    int result = x + y;
+    return result;
+}
+
+void sum1(int x, int y)
+{
+    printf("void 함수 = 두 값의 합은 %d\n", x + y);
+}
+
+int main()
+{
+    int x = 3, y = 5;
+    int result;
+    result = sum(x, y);
+    printf("return 함수 = 두 값의 합은 %d\n", result);
+    sum1(x, y);
+}
+
---
+++ C문법/함수2/함수2.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+using namespace std;
+
+void funcPrint()
+{
+    cout << "funcPrint 호출" << endl;
+}
+
+void paraPrint(int x)
+{
+    cout << "매개변수에 전달받은 값 : " << x << endl;
+}
+
+void paraPrint(int x, double y)
+{
+    cout << "매개변수 x에 전달받은 값 : " << x << endl;
+    cout << "매개변수 y에 전달받은 값 : " << y << endl;
+}
+
+void ChangeValue(int x)
+{
+    cout << "x의 값 : " << x << endl;
+    x += 10;
+    cout << "x의 값 : " << x << endl;
+}
+
+void ChangeValuePointer(int& x)
+{
+    x += 10;
+}
+
+void ChangePtr(int*& x)
+{
+    x = nullptr;
+}
+
+int main()
+{
+    funcPrint();
+    paraPrint(3);
+    paraPrint(4, 10.1);
+
+    int arg1 = 1;
+    printf("arg1의 값 : %d\n", arg1);
+    ChangeValue(arg1);
+    printf("arg1의 값 : %d\n", arg1);
+
+    printf("함수 호출 전 arg1의 값 : %d\n", arg1);
+    ChangeValuePointer(arg1);
+    printf("함수 호출 전 arg1의 값 : %d\n", arg1);
+
+    int arg = 1;
+    int* int_ptr = &arg;
+    printf("함수 호출전 int_ptr의 값 : %p\n", int_ptr);
+    ChangePtr(int_ptr);
+    printf("함수 호출후 int_ptr의 값 : %p\n", int_ptr);
+
+    return 0;
+}
+
