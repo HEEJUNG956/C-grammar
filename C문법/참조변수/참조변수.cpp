457bf255c54c98b6ef5fe85346f87175f858a455
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-06-20 13:05:52 +0900

    참조변수

---
+++ C문법/참조변수/참조변수.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+    //const int num = 10; const로 선언된 변수는 non-const로만 참조가능
+    int num = 10;
+    int& refer = num;
+    //int& refer2; 참조 변수는 선언과 동시에 초기화하여야한다
+    //int& refer3 = NULL; null을 저장할수있는 포인터와 다르게 null은 참조가 불가능
+    const int& refer4 = num;
+    //refer4 = a; 한번 참조한 변수는 재참조 불가능
+
+    printf("일반 변수 호출 : %d 참조 변수 호출 : %d \n", num, refer);
+    refer = 20;
+    printf("일반 변수 호출 : %d 참조 변수 호출 : %d \n", num, refer);
+    ++num;
+    printf("일반 변수 호출 : %d 참조 변수 호출 : %d \n", num, refer);
+    printf("\n------------------------------------------------------\n");
+    printf("일반 변수 호출 : %d 참조 변수 호출 : %d \n", num, refer4);
+    return 0;
+}
+
---
+++ C문법/참조변수2/참조변수2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+struct Human
+{
+    string name;
+    int age;
+    double tall;
+};
+void ChangeStruct(Human& h, string n, int a, double t)
+{
+    h.name = n;
+    h.age = a;
+    h.tall = t;
+}
+int main()
+{
+    Human human1 = { "김OO", 26, 179.5 };
+    printf("%s %d %.2f\n", human1.name.c_str(), human1.age, human1.tall);
+    ChangeStruct(human1, "박OO", 100, 169.5);
+    printf("%s %d %.2f", human1.name.c_str(), human1.age, human1.tall);
+    printf("\n------------------------------------------------\n");
+
+    int num = 5;
+    int* ptr = &num;    // num의 포인터(메모리 주소값)
+    int& ref = num;     // num의 참조
+
+    printf("%p\n", ptr);
+    printf("%d\n", ref);
+
+}
