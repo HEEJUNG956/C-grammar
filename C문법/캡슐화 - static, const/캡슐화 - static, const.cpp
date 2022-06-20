ebc297916a2dbdb9a629d5bbaa63039ab638598f
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-06-20 15:33:37 +0900

    캡슐화

---
+++ C문법/캡슐화 - static, const/캡슐화 - static, const.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+class Test
+{
+    int num1;
+    static int a;
+public:
+    Test() : num1(10) {};
+    void PrintVal()
+    {
+        printf("일반 멤버 변수 : %d\n", num1);
+        printf("static 멤버 변수 : %d\n", a);
+        printf("\n========================================================\n");
+    }
+    void setStatic(int x)
+    {
+        a = x;
+    };
+    void setNum(int x)
+    {
+        num1 = x;
+    }
+};
+
+int Test::a = 0;
+
+int main()
+{
+    Test t1;
+    Test t2;
+    
+    t1.PrintVal();
+    t2.PrintVal();
+
+    t1.setStatic(100);
+    t2.setNum(22);
+
+    t1.PrintVal();
+    t2.PrintVal();
+}
+
---
+++ C문법/캡슐화/캡슐화 - 프렌드.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+using namespace std;
+
+//class F_test1
+//{
+//    int a;
+//    friend class F_test2;
+//};
+//
+//class F_test2
+//{
+//public:
+//    void Set(F_test1& F, int x)
+//    {
+//        F.a = x;
+//    }
+//    void PrintInfo(F_test1& F)
+//    {
+//        printf("private로 선언된 F_test1의 a의 값 : %d\n", F.a);
+//    }
+//};
+
+class F_test1;
+
+class F_test2
+{
+public:
+    void PrintInfo(F_test1&);
+};
+
+class F_test1
+{
+    int a;
+public:
+    F_test1(int _a = 0) : a(_a) {};
+    friend void F_test2::PrintInfo(F_test1& F);
+};
+
+void F_test2::PrintInfo(F_test1& F)
+{
+    cout << F.a << endl;
+}
+
+class F_temp1
+{
+    int b;
+public:
+    F_temp1(int _b = 0) : b(_b) {};
+    friend void TempPrintInfo(F_temp1&);
+};
+
+void TempPrintInfo(F_temp1& T)
+{
+    cout << T.b << endl;
+}
+
+int main()
+{
+    //F_test1 f1;
+    //F_test2 f2;
+
+    //f2.Set(f1, 7);
+    //f2.PrintInfo(f1);
+
+    F_test1 f1(5);
+    F_test2 f2;
+    f2.PrintInfo(f1);
+
+    F_temp1 t1(5);
+    TempPrintInfo(t1);
+    return 0;
+}
+
