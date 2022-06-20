76b92ffa2b048738450a2e706c238d692fea56d6
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-06-20 10:27:51 +0900

    공용체 / 열거체

---
+++ C문법/공용체(union)/공용체(union).cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+union Share
+{
+    int a;
+    double b;
+    char c;
+};
+
+int main()
+{
+    Share data;
+    data.a = 67;
+
+    printf("data.a의 값 : %d\n", data.a);
+    printf("data.b의 값 : %f\n", data.b);
+    printf("data.c의 값 : %c\n", data.c);
+
+    printf("data.a의 값 : %d\n", sizeof(data.a));
+    printf("data.a의 값 : %d\n", sizeof(data.b));
+    printf("data.a의 값 : %d\n", sizeof(data.c));
+}
+
---
+++ C문법/열거체(enum)/열거체(enum).cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+enum color
+{
+    red,
+    blue,
+    green
+};
+
+enum Weather
+{
+    sunny,
+    cloud,
+    rainy,
+    snowy
+};
+
+int main()
+{
+    color col;
+    col = blue;
+    
+    switch (col)
+    {
+    case red:
+        printf("빨강색입니다.\n");
+        break;
+    case blue:
+        printf("파란색입니다\n");
+        break;
+    case green:
+        printf("초록색입니다\n");
+        break;
+    default:
+        printf("잘못된 값입니다.\n");
+        break;
+    }
+}
