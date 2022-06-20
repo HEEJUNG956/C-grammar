b25e969e3259b3112c88516dfcf022a868ae7ec4
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-06-20 11:55:19 +0900

    함수 / 오버로딩 라이딩

---
+++ C문법/오버로딩 오버라이딩/오버로딩 오버라이딩.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+void average(int a, int b)
+{
+    int avg = 0;
+    avg = (a * b) / 2;
+    printf("정수값 %d와 %d의 평균은 %d입니다.\n", a, b, avg);
+}
+
+void average(float a, float b)
+{
+    float avg = 0;
+    avg = (a * b) / 2;
+    printf("실수값 %f와 %f의 평균은 %f입니다.\n", a, b, avg);
+}
+
+class Parent
+{
+public:
+    void outA(int a)
+    {
+        printf("%d\n", a);
+    }
+};
+
+class Child :public Parent
+{
+public:
+    void outA(int a)
+    {
+        printf("%d\n", a + a);
+    }
+};
+
+int main()
+{
+    Child a;
+    Parent b;
+    a.outA(10);
+    b.outA(10);
+}
+
---
+++ C문법/함수 다양하게/함수 다양하게.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void PrintVal(int n1, double n2, char n3 = 'a', string n4 = "Hi")
+{
+    printf("n1 : %d\n", n1);
+    printf("n2 : %.1f\n", n2);
+    printf("n3 : %c\n", n3);
+    printf("n4 : %s\n", n4.c_str());
+    printf("\n---------------------------------------------------\n");
+}
+
+inline int Sum(int x, int y)
+{
+    return x + y;
+}
+
+inline int Sub(int x, int y)
+{
+    return x - y;
+}
+
+int main()
+{
+    PrintVal(1, 10.1);
+    PrintVal(1, 10.1, 'F');
+    PrintVal(1, 10.1, 'F', "Hello");
+
+    int a = 13, b = 5;
+    printf("두 수의 합 : %d\n", Sum(a, b));
+    printf("두 수의 차 : %d\n", Sub(a, b));
+    return 0;
+}
