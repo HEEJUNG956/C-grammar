7f16abbaf2e98ee01eddbd40f103f401a9eeeba9
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-06-20 10:24:41 +0900

    구조체

---
+++ C문법/구조체/구조체.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Student {
+    string name;
+    string school;
+    string major;
+    int age;
+    int grade;
+    int stu_num;
+};
+
+int main()
+{
+    Student stu1;
+    stu1.name = "홍길동";
+    stu1.school = "대학교";
+    stu1.major = "수학과";
+    stu1.age = 26;
+    stu1.grade = 4;
+    stu1.stu_num = 20220620;
+    
+    Student stu2 = { "철수", "중학교", "사립", 15, 2, 0000001 };
+    Student stu3 = { "영희", "고등학교", "국립" };
+    
+    cout << "이름 : " << stu1.name << "\n학교 : " << stu1.school << "\n전공 : " << stu1.major;
+    cout << "\n나이 : " << stu1.age << "\n학년 : " << stu1.grade << "\n학번 : " << stu1.stu_num;
+    cout << "\n----------------------------------------\n";
+    cout << "이름 : " << stu2.name << "\n학교 : " << stu2.school << "\n전공 : " << stu2.major;
+    cout << "\n나이 : " << stu2.age << "\n학년 : " << stu2.grade << "\n학번 : " << stu2.stu_num;
+    cout << "\n----------------------------------------\n";
+    cout << "이름 : " << stu3.name << "\n학교 : " << stu3.school << "\n전공 : " << stu3.major;
+    cout << "\n나이 : " << stu3.age << "\n학년 : " << stu3.grade << "\n학번 : " << stu3.stu_num;
+
+    return 0;
+}
+
+
+
---
+++ C문법/구조체2/구조체2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Employee
+{
+    string name;
+    int employee_num;
+};
+
+struct Company
+{
+    string locate;
+    int revenue;
+    Employee employee1;
+};
+
+struct Test
+{
+    int test1;
+    char test2;
+    double test3;
+};
+
+int main()
+{
+    Company myCom = { "서울", 10000, {"홍길동", 00001} };
+
+    printf("회사 위치 : %s\n", myCom.locate.c_str());
+    printf("사원 이름 : %s\n", myCom.employee1.name.c_str());
+
+
+    Test test;
+    cout << "멤버변수의 크기 : " << sizeof(test.test1) << ',' << sizeof(test.test2);
+    cout << ',' << sizeof(test.test3) << endl;
+
+    cout << "구조체의 크기 : " << sizeof(test) << endl;
+}
