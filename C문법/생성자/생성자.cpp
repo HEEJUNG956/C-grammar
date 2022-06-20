ba026c09cad3eb76b0dad76314903251d832fdaa
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-06-20 14:38:54 +0900

    생성자 / 소멸자

---
+++ C문법/생성자/생성자.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+using namespace std;
+
+// 기본 생성자
+class Test
+{
+    int num1;
+    double num2;
+public:
+    Test(int x, double y)
+    {
+        printf("생성자 호출\n");
+        num1 = x;
+        num2 = y;
+    }
+    
+    void printNum() const
+    {
+        printf("num1 : %d\n", num1);
+        printf("num2 : %.1f\n", num2);
+    }
+
+    // 복사 생성자
+    Test(Test& t)
+    {
+        printf("복사 생성자 호출\n");
+        num1 = t.num1;
+        num2 = t.num2;
+    }
+
+    // 멤버 이니셜라이저
+    Test():num1(10), num2(10.1){}
+    
+};
+
+// 디폴트 생성자
+class Temp
+{
+    int num1;
+    double num2;
+public:
+    Temp(int a = 10, double b = 10.1)
+    {
+        printf("디폴트 생성자 호출\n");
+        num1 = a;
+        num2 = b;
+    }
+    void printNum() const
+    {
+        printf("num1 : %d\n", num1);
+        printf("num2 : %.1f\n", num2);
+    }
+};
+
+
+
+
+
+int main()
+{
+    Test test1(10, 10.1);
+    test1.printNum();
+    printf("\n================================================================\n");
+    Test test2(test1);
+    test2.printNum();
+    printf("\n================================================================\n");
+    Temp temp;
+    temp.printNum();
+}
---
+++ C문법/소멸자/소멸자.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+//소멸자
+class Test
+{
+    int num1;
+    double num2;
+    int* pt_num;
+public:
+    Test() : num1(10), num2(10.1)
+    {
+        pt_num = new int;
+        *pt_num = 10;
+        printf("생성자 호출\n");
+    };
+    ~Test()
+    {
+        delete pt_num;  // 메모리 제거
+        printf("소멸자 호출\n");
+    }
+};
+int main()
+{
+    Test test1;
+}
+
