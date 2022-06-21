1f16e76ae24160f07ce1d4b74b72faa78bc66dac
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-06-21 14:29:22 +0900

    일반 정적 상수 변수

---
+++ C문법/캡슐화 - static, const 2/캡슐화 - static, const 2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+class Test 
+{
+    int num1;
+    static int num2;
+    const int num3;
+public:
+    Test() :num1(10), num3(20) {};
+    void printVal() const
+    {
+        printf("일반 멤버 변수 : %d\n", num1);
+        printf("정적 멤버 변수 : %d\n", num2);
+        printf("상수 멤버 변수 : %d\n", num3);
+    }
+};
+
+int Test::num2 = 0;
+
+int main()
+{
+    Test t;
+    t.printVal();
+}
+
