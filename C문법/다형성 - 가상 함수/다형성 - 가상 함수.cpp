8b0fa7afac7bc27aa26723f18fba46055d6e6103
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-06-22 09:10:37 +0900

    다형성

---
+++ C문법/다형성 - 가상 함수/다형성 - 가상 함수.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+using namespace std;
+class Car
+{
+public:
+    virtual void Speed() = 0;
+};
+
+class NormalCar : public Car
+{
+public:
+    virtual void Speed()
+    {
+        printf("보통\n");
+    }
+};
+
+class SuperCar : public Car
+{
+public:
+    virtual void Speed()
+    {
+        printf("Fast\n");
+    }
+};
+
+// 가상 소멸자
+class A
+{
+public:
+    A()
+    {
+        printf("부모클래스 생성자\n");
+    }
+    virtual ~A()
+    {
+        printf("부모클래스 소멸자\n");
+    }
+};
+
+class B :public A
+{
+public:
+    B()
+    {
+        printf("자식클래스 생성자\n");
+    }
+    virtual ~B()
+    {
+        printf("자식클래스 소멸자\n");
+    }
+};
+
+
+int main()
+{
+    Car* pt_car;
+    SuperCar scar;
+    NormalCar ncar;
+
+    pt_car = &scar;
+    pt_car->Speed();
+
+    pt_car = &ncar;
+    pt_car->Speed();
+
+    printf("\n");
+    A* pt_a;
+    B* b = new B;
+
+    pt_a = b;
+    delete pt_a;
+}
