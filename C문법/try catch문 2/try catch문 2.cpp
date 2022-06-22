4c9df9fc84bcc477b3bc6bab4faee8efce6bc0d9
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-06-22 09:32:01 +0900

    try catch

---
+++ C문법/try catch문 2/try catch문 2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    try
+    {
+        int level = 2;
+        if (level >= 3)
+        {
+            printf("입장 가능");
+        }
+        else
+        {
+            throw 505;
+        }
+    }
+    catch (...)
+    {
+        printf("접근불가. level 3 이상 입장 가능");
+    }
+}
+
---
+++ C문법/try catch문/try catch문.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+int	main(void)
+{
+    try
+    {
+        int level = 1;
+        if (level >= 3)
+        {
+            printf("입장 가능");
+        }
+        else
+        {
+            throw 505;
+        }
+    }
+    catch (int error)
+    {
+        printf("접근 불가. level 3 이상 입장 가능.\n");
+        printf("에러번호 : %d", error);
+    }
+    return 0;
+}
