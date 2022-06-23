74fdee30b275e8b9b0b4ccf5113be04fac85f878
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-06-23 09:17:56 +0900

    STL

---
+++ C문법/STL - 템플릿 - 클래스템플릿/STL - 템플릿 - 클래스템플릿.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+template<typename T>
+class Test
+{
+    T val;
+public:
+    Test(T v) :val(v) {};
+    T GetVal();
+    void PrintInfo()
+    {
+        cout << "멤버변수 : " << val << endl;
+    }
+};
+
+template<typename T>
+T Test<T>::GetVal()
+{
+    return val;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
---
+++ C문법/STL - 템플릿 - 함수템플릿/STL - 템플릿 - 함수템플릿.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+template<typename T>
+T Sum(T a, T b)
+{
+    return a + b;
+}
+
+template<typename T1, typename T2>
+void ShowInfo(T1 a, T1 b)
+{
+    cout << "T1 타입 : " << typeid(a).name() << endl;
+    cout << "매개변수 a : " << a << endl;
+    cout << "T2 타입 : " << typeid(b).name() << endl;
+    cout << "매개변수 b : " << b << endl;
+}
+
+template<typename S1, typename S2>
+void ShowInfo(S1 s1, S2 s2)
+{
+    cout << "S1 : " << s1 << endl;
+    cout << "S2 : " << s2 << endl;
+}
+
+template<>
+void ShowInfo<double, int>(double a, int b)
+{
+    cout << "특정 동작 " << endl;
+}
+int main()
+{
+    string a = "ab", b = "cd";
+    printf("%d\n", Sum(10, 20));
+    printf("%.2f\n", Sum(3.14, 2.3));
+    printf("%s\n", Sum(a, b).c_str());
+
+    printf("\n==========================================\n");
+    printf("%s", Sum<string>("ab", "cd").c_str());
+
+    printf("\n==========================================\n");
+
+    ShowInfo<int, double>(10, 20.2);
+    
+    ShowInfo<int, string>(10, "Hi");
+    ShowInfo<double, int>(22.2, 11);
+}
+
